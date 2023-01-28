6d23828a6683ea2984967ac23a0d78b4e11921d4
xuyaohui <972918183@qq.com>
2023-01-28 16:27:33 +0800

    init

---
+++ src/main/java/com/xu/flink/SpringbootMongodbVueApplication.java
@@ -0,0 +1,13 @@
+package com.xu.flink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMongodbVueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xu/flink/SpringbootMongodbVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xu.flink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
